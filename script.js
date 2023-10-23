b0c2ec8a2bf178f3375053d501cba935b69c925e
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-10-23 09:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+	let body = document.getElementsByTagName('body')[0];
+	body.textContent = "DOM load success";
+})
